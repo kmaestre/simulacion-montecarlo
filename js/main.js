6b0a526a9e2fa8d05860e9432d68f502b729f924
Miguel Maestre <maestre.95@gmail.com>
2019-03-18 22:12:42 -0400

    cambios menores

--- js/main.js
+++ js/main.js
@@ -20,23 +20,31 @@ const ejecutar = () => {
 	if (metodo == 'cuamed') {	
 		let sem = document.getElementById('sem').value
 		let NPA = (cuadradoMedio(sem, sem.toString().length, 0, []))
+		let aleatorios = []
+		NPA.forEach(fila => {aleatorios.push(parseFloat(fila[fila.length-1]))})
 		tablaCuadradoMedio(NPA)
 	}
 	if (metodo == 'promed') {	
 		let sem1 = document.getElementById('sem1').value
 		let sem2 = document.getElementById('sem2').value
 		let NPA = (productoMedio(sem1, sem2, sem1.length, 0, []))
+		let aleatorios = []
+		NPA.forEach(fila => {aleatorios.push(parseFloat(fila[fila.length-1]))})
 		tablaProductoMedio(NPA)
 	}
 	if (metodo == 'promed2') {	
 		let sem = document.getElementById('sem').value
 		let a = document.getElementById('a').value
 		let NPA = (productoMedioVariado(sem, a, sem.length, 0, []))
+		let aleatorios = []
+		NPA.forEach(fila => {aleatorios.push(parseFloat(fila[fila.length-1]))})
 		tablaProductoMedioVariado(NPA)
 	}
 	if (metodo == 'conmix') {	
 		let sem = document.getElementById('sem').value
 		let NPA = (congruencialMixto(sem, sem.length, 0, []))
+		let aleatorios = []
+		NPA.forEach(fila => {aleatorios.push(parseFloat(fila[fila.length-1]))})
 		tablaCongruencialMixto(NPA)
 	}
 	if (metodo == 'conmul') {
